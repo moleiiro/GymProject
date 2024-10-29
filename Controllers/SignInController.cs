64b1edf01fad3c39aa07ed2e2cf03980ae722d62
moleiiro <220000538@esg.ipsantarem.pt>
2024-10-29 22:53:56 +0000

    initial commit

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymProject.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index() 
+        { 
+            return View(); 
+        }
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+        public IActionResult PersonalTrainer()
+        {
+            return View();
+        }
+    }
+
+}
